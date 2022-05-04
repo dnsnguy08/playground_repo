9bae82f459e1c9a032d9f7646d928ff881b23685
Dennis Nguyen <dnsnguy08@gmail.com>
2022-05-03 21:32:42 -0700

    Week 3: Init JS Activities

---
+++ Javascript/01-Activities/04-Stu_Hello-Variable/Unsolved/script.js
@@ -0,0 +1,16 @@
+// Write Your JavaScript Code Here
+var personName = 'Dennis';
+var pets = 2;
+var funFact = 'I like fishing';
+
+console.log("My name is " + personName + ".");
+console.log("I have " + pets + " pet(s).");
+console.log("Fun fact: " + funFact + ".");
+
+personName = 'Vi';
+pets = 3;
+funFact = 'I like eating';
+
+console.log("My name is " + personName + ".");
+console.log("I have " + pets + " pet(s).");
+console.log("Fun fact: " + funFact + "."); 
---
+++ Javascript/01-Activities/06-Stu_Primitive-Types/Unsolved/script.js
@@ -0,0 +1,23 @@
+var one = 64;
+var two = "656302";
+var three = false;
+var four = 64.55;
+var five = "Howdy!";
+var six;
+
+// Insert comments to explain what each console log below will log to the console
+console.log(typeof one); // expected output: number
+console.log(typeof two); // expected output: string
+console.log(typeof three); // expected output: bool
+console.log(typeof four); // expected output: number
+console.log(typeof five); // expected output: string
+console.log(typeof six); // expected output: undefined
+
+four = "Hello!";
+five = false;
+six = 23;
+
+// Insert comments to explain what each console log below will log to the console
+console.log(typeof four); // expected output: string
+console.log(typeof five); // expected output: bool
+console.log(typeof six); // expected output: number
