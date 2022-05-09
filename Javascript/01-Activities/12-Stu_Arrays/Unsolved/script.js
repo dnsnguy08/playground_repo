feb1b919c484e4e89c683f18e0ca09b408b07226
Dennis Nguyen <dnsnguy08@gmail.com>
2022-05-08 18:03:31 -0700

    Week 3: JS Arrays

--- Javascript/01-Activities/12-Stu_Arrays/Unsolved/script.js
+++ Javascript/01-Activities/12-Stu_Arrays/Unsolved/script.js
@@ -1,1 +1,17 @@
 // WRITE YOUR CODE HERE
+var names = [
+    'Dennis',
+    'Daniel',
+    'Kenny',
+    'Matthew',
+]
+
+for (let i = 0, len = names.length, text = "Welcome to the class ", new_names = []; i < len; i++) {
+    console.log(text + names[i]);
+}
+
+names[0] = 'Manny';
+
+if (names[0] !== 'Dennis') {
+    console.log(`${snames[0]} is in class`);
+}
