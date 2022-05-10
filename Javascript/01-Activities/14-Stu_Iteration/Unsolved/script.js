ca4536158cc0ab9e5faca5e5bc50ed6cb6099b5e
Dennis Nguyen <dnsnguy08@gmail.com>
2022-05-09 21:57:54 -0700

    Week 3: JS Iteration

--- Javascript/01-Activities/14-Stu_Iteration/Unsolved/script.js
+++ Javascript/01-Activities/14-Stu_Iteration/Unsolved/script.js
@@ -1,1 +1,17 @@
 //WRITE YOUR CODE BELOW
+
+var students = [
+    'Daniel',
+    'Jining',
+    'Uday',
+    '4',
+    '5'
+]
+
+console.log(students.length);
+
+text = "Great to see you "
+
+for (let i = 0, len = students.length; i < len; i++) {
+    console.log(text + students[i]);
+}
