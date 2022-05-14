a98223cbe8736d1354f29c0b979fdf09ec4dc166
Dennis Nguyen <dnsnguy08@gmail.com>
2022-05-13 22:08:29 -0700

    Week 3: JS Object methods activities

--- Javascript/01-Activities/24-Stu_Object-Methods/Unsolved/script.js
+++ Javascript/01-Activities/24-Stu_Object-Methods/Unsolved/script.js
@@ -9,22 +9,31 @@ var shelter = {
 };
 
 //Debug the code below 
+// removed .Lulu property because chosenPet is a string
+// add shelter object
 function dogMessage() {
-   console.log("Congrats! " + chosenPet.Lulu + ", a great dog, is available for adoption!");
-   apptMessage();
+   console.log("Congrats! " + chosenPet + ", a great dog, is available for adoption!");
+   shelter.apptMessage();
 }
 
+// add parentheses to apptMessage method
 function catMessage() {
   console.log("Congrats! " + chosenPet + ", an awesome cat, is available for adoption!");
-  shelter.apptMessage;
+  shelter.apptMessage();
 }
 
-if (dogs.includes(chosenPet)) {
+// add shelter. object to use access the properties
+if (shelter.dogs.includes(chosenPet)) {
   dogMessage();
-} else if (cats.includes(chosenPet)) {
+} else if (shelter.cats.includes(chosenPet)) {
   catMessage();
 } else {
   console.log("It looks like the pet is not available.");
-  console.log("Check out our featured dog, " + shelter[0] + ". or our featured cat, " + cat.shelter[1]);
+  console.log("Check out our featured dog, " + shelter.dogs[0] + ". or our featured cat, " + shelter.cats[1]);
 }
   
+// Bonus: create array from Object.values()
+
+var createArray = Object.values(shelter);
+
+console.log(createArray);
