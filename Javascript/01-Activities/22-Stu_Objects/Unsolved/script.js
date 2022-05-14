8565833a34b7b1190282efe32d4da635c98d9bbe
Dennis Nguyen <dnsnguy08@gmail.com>
2022-05-13 22:07:57 -0700

    Week 3: JS Objects activities

--- Javascript/01-Activities/22-Stu_Objects/Unsolved/script.js
+++ Javascript/01-Activities/22-Stu_Objects/Unsolved/script.js
@@ -1,1 +1,18 @@
 //WRITE YOUR CODE BELOW
+var customerOrder = {
+    drink: 'Dr. Pepper',
+    sugar: '10g',
+    isReady: false
+};
+
+// log sugar, drink name to console:
+console.log(customerOrder.drink);
+
+console.log(customerOrder.sugar);
+
+// log order ready if true
+if (customerOrder.isReady) {
+    console.log("Ready for pick-up");    
+} else {
+    console.log("Still in order queue");    
+}
