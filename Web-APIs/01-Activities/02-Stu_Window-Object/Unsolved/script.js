5114f9605674ecd9aa6afd6157748d8ae3287571
Dennis Nguyen <dnsnguy08@gmail.com>
2022-05-16 18:11:12 -0700

    Week 4: Web API window object

--- Web-APIs/01-Activities/02-Stu_Window-Object/Unsolved/script.js
+++ Web-APIs/01-Activities/02-Stu_Window-Object/Unsolved/script.js
@@ -1,12 +1,16 @@
 // TODO: What will the following line of code log?
+// Log the object & its methods of which the code is running on
 console.log(window);
 
 // TODO: What will the following line of code log?
+// Log the the webpage itself and its elements
 console.log(window.document);
 
 // TODO: What will the following line of code log?
+// Log the root node of the document.
 console.log(document.documentElement);
 
 // TODO: What will the following line of code log?
+//Log the head element of the page
 console.log(document.head);
 
