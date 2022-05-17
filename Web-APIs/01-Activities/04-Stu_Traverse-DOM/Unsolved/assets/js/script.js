58c8b4c5313471fa6b2a034aa231704560f8f4f6
Dennis Nguyen <dnsnguy08@gmail.com>
2022-05-16 18:11:55 -0700

    Week 4: Web API DOM object

--- Web-APIs/01-Activities/04-Stu_Traverse-DOM/Unsolved/assets/js/script.js
+++ Web-APIs/01-Activities/04-Stu_Traverse-DOM/Unsolved/assets/js/script.js
@@ -3,5 +3,14 @@ var articlesDiv = document.getElementById('articles');
 var headerDiv = document.getElementById('header');
 
 // Change style by accessing style object's properties
-articlesDiv.children[2].style.fontSize = '50px';
+articlesDiv.children[0].style.fontSize = '50px';
 headerDiv.style.color = 'white';
+
+// Bonus:
+
+// Gets	Selector Syntax	Method
+// ID	#demo	getElementById()
+// Class	.demo	getElementsByClassName()
+// Tag	demo	getElementsByTagName()
+// Selector (single)		querySelector()
+// Selector (all)		querySelectorAll()
