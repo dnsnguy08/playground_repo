2798811011326159e975efe6e941beb0089e160d
Dennis Nguyen <dnsnguy08@gmail.com>
2022-05-16 18:10:30 -0700

    Week 3: JS This Object activity

--- Javascript/01-Activities/26-Stu_Object-This/Unsolved/script.js
+++ Javascript/01-Activities/26-Stu_Object-This/Unsolved/script.js
@@ -1,12 +1,16 @@
 // TODO: What does 'this' refer to?
+// keyword "this" refers to the global object
+// In a browser, the global object is the Window 
 console.log(this);
 
 // TODO: What does 'this' refer to?
+//'this' refers to the global object window - since it is not defined within function
 function helloThis() { 
    console.log("Inside this function, this is " + this); 
    }
 
 // TODO: What will this log? 
+// 'this' refers to the 'child' object, 'child.age + 10 = 20'
 var child = { 
    age: 10,
    ageTenYears: function() {
@@ -15,6 +19,7 @@ var child = {
 };
 
 // TODO: What will this log? 
+// 'this' refers to investment property, 'investment.initialInvestment * 1.15 = 5750'
 var investor = {  
   name: "Cash Saver",
   investment: {
@@ -31,3 +36,22 @@ helloThis();
 // TODO: After commenting, check your results!
 child.ageTenYears();
 investor.investment.investmentGrowth();
+
+
+
+// Bonus: randomly select an element from an array
+
+// Use Math. random() function to get the random number between(0-1, 1 exclusive).
+// Multiply it by the array length to get the numbers between(0-arrayLength).
+// Use Math. floor() to get the index ranging from(0 to arrayLength-1).
+
+
+var items = [
+   1,
+   2,
+   3,
+   4,
+   5
+]
+var item = items[Math.floor(Math.random()*items.length)];
+console.log(item);
