3ab858c0a4ce0c66cacf1fdf48cbb82b8b96c931
Dennis Nguyen <dnsnguy08@gmail.com>
2022-07-02 17:04:16 -0700

    OOP constructors

--- OOP/01-Activities/02-Stu_Constructors/Unsolved/index.js
+++ OOP/01-Activities/02-Stu_Constructors/Unsolved/index.js
@@ -1,5 +1,15 @@
 // TODO: Create a constructor function called 'Developer' that takes in 'name' and 'tech'
+function Developer(name, tech){
+    this.name = name;
+    this.tech = tech;
+    this.introduction = () => {
+        console.log(`Hi my name is ${this.name} and my favorite tech is ${this.tech}`);
+      };
+}
 
+const me = new Developer('Dennis', 'automation');
+
+me.introduction();
 // TODO: Include a method called 'introduction()' that introduces the Developer with their name and favorite tech
 
 // TODO: Create a new object using the 'Developer' constructor
