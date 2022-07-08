c169f7e733cf88a06993496fdfaa02859c43b532
Dennis Nguyen <dnsnguy08@gmail.com>
2022-07-07 18:00:12 -0700

    finished node module activities

--- OOP/01-Activities/12-Stu_TDD/Unsolved/algo.js
+++ OOP/01-Activities/12-Stu_TDD/Unsolved/algo.js
@@ -1,9 +1,22 @@
 function Algo() {}
 
-Algo.prototype.reverse = function(str) {};
+Algo.prototype.reverse = function(str) {
+    const reverseString = str.split("").reverse().join("");
+    return reverseString;
+};
 
-Algo.prototype.isPalindrome = function(str) {};
+Algo.prototype.isPalindrome = function(str) {
+    const reverseString = str.split("").reverse().join("");
+    return str === reverseString;
+};
 
-Algo.prototype.capitalize = function(str) {};
+Algo.prototype.capitalize = function(str) {
+    const stringArr = str.split(" ");
+    for (let i = 0; i < stringArr.length; i++) {
+        stringArr[i] = stringArr[i].charAt(0).toUpperCase() + stringArr[i].slice(1);
+    }
+    const string = stringArr.join(" ");
+    return string;
+};
 
 module.exports = Algo;
--- OOP/01-Activities/12-Stu_TDD/Unsolved/test/algo.test.js
+++ OOP/01-Activities/12-Stu_TDD/Unsolved/test/algo.test.js
@@ -3,13 +3,31 @@ const Algo = require("../algo");
 describe("Algo", () => {
   describe("reverse", () => {
     // TODO: Write a test for the `reverse` method that should take a string as an argument and return a new reversed version of the string
+    it("should return a string in reverse order", () => {
+      const string = 'hello';
+      const result = new Algo().reverse(string);
+      
+      expect(result).toEqual('olleh');
+    });
   });
 
   describe("isPalindrome", () => {
     // TODO: Write a test for the `isPalindrome` method that should take a string as an argument and return the boolean `true` if the provided string is a palindrome.
+    it("should return true if a word that is a palindrome is passed into isPalindrome", () => {
+      const string = 'racecar';
+      const result = new Algo().isPalindrome(string);
+      
+      expect(result).toEqual(true);
+    });
   });
 
   describe("capitalize", () => {
     // TODO: Write a test for the `capitalize` method that should take a string as an argument and return a new string with the first letter of each word capitalized
+    it("should return a new string with every word capitalized", () => {
+      const string = 'hello world';
+      const result = new Algo().capitalize(string);
+      
+      expect(result).toEqual('Hello World');
+    });
   });
 });
