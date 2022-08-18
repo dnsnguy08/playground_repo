e62d347ce4ff2dfd121ef33fc3c280f9135c5cf6
Dennis Nguyen <dnsnguy08@gmail.com>
2022-08-17 21:48:24 -0700

    adding sql todos practice

---
+++ mysqlTodos/model/seeds.sql
@@ -0,0 +1,9 @@
+USE todos_db;
+
+INSERT INTO todos (todo, isCompleted)
+VALUES ('Run', true),
+       ('Wash Dishes', false),
+       ('Fold Laundry', true),
+       ('Print Money', true),
+       ('Watch TV', false),
+       ('Eat Dinner', false);
