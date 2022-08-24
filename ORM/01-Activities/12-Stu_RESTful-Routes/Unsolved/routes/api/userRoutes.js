6cb804874d419a8358512b3a30085aa730afd543
Dennis Nguyen <dnsnguy08@gmail.com>
2022-08-23 19:06:22 -0700

    finished ORM activities

--- ORM/01-Activities/12-Stu_RESTful-Routes/Unsolved/routes/api/userRoutes.js
+++ ORM/01-Activities/12-Stu_RESTful-Routes/Unsolved/routes/api/userRoutes.js
@@ -6,30 +6,46 @@ const User = require('../../models/User');
 
 // GET a user
 router.get('/:id', async (req, res) => {
-  const userData = await User.findByPk(req.params.id).catch((err) =>
-    res.json(err)
-  );
-  res.json(userData);
+  try {
+    const userData = await User.findByPk(req.params.id);
+    if (!user) {
+      return res.status(404).json({error: 'No user found'});
+    }
+    res.json(userData);
+  } catch (error) {
+    res.status(500).json({error});
+  }
 });
 
 // UPDATE a user
 router.put('/:id', async (req, res) => {
-  const userData = await User.update(req.body, {
-    where: {
-      id: req.params.id,
-    },
-  }).catch((err) => res.json(err));
-  res.json(userData);
+  try {
+    const userData = await User.update(req.body, {
+      where: {
+        id: req.params.id,
+      }
+    })
+    if (!user) {
+      return res.status(404).json({error: 'No user found'});
+    }
+    res.json(userData);
+  } catch (error) {
+    res.status(500).json({error});
+  }
 });
 
 // DELETE a user
 router.delete('/:id', async (req, res) => {
-  const userData = await User.destroy({
-    where: {
-      id: req.params.id,
-    },
-  }).catch((err) => res.json(err));
-  res.json(userData);
+  try {
+    const userData = await User.destroy({
+      where: {
+        id: req.params.id,
+      },
+    })
+
+  } catch (error) {
+    res.status(500).json({error});
+  }
 });
 
 module.exports = router;
