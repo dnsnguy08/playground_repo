98b6b68504888aac97f92516360ad650110f86ae
Dennis Nguyen <dnsnguy08@gmail.com>
2022-09-07 11:11:51 -0700

    react day 1

---
+++ React/react-playground/lectures/App.js
@@ -0,0 +1,13 @@
+import HelloWorld from "./helloWorld";
+function App() {
+  return (
+    <>
+    <HelloWorld
+    className = 'red'
+    id = 'dennis'
+    dennis = 'cool guy'/>
+    </>
+  );
+}
+
+export default App;
---
+++ React/react-playground/lectures/helloWorld.js
@@ -0,0 +1,6 @@
+const HelloWorld = (props) => {
+    console.log(props);
+    return <h1>Hello World</h1>;
+}
+
+export default HelloWorld;
